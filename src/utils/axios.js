dd67b07c3ed598c524871181ee4490380a7e370b
rusmiarahaman77461@gmail.com <rusmiarahaman77461@gmail.com>
2026-07-01 13:26:22 +0600

    refactor: configure centralized axios instance with local environmental api targets and session cookie credentials

---
+++ src/utils/axios.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL:      import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  withCredentials: true,   // sends Better Auth session cookies automatically
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => Promise.reject(err)
+);
+
+export default api;
